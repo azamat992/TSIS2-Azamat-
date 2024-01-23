0149d0c581ff80c531972414259d2cd544e42183
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 17:36:26 +0600

    Create 1Booleans.py

---
+++ 1Booleans.py
@@ -0,0 +1,26 @@
+#Exercise 1 Azamat
+#The statement below would print a Boolean value, which one? Azamat
+print(10 > 9)
+#Resullt:True
+
+#Exercise 2 Azamat
+#The statement below would print a Boolean value, which one?
+print(10 == 9)
+#Result:False
+
+#Exercise 3 Azamat
+#The statement below would print a Boolean value, which one?
+print(10 < 9)
+#Result:False
+
+#Exercise 4 Azamat
+#The statement below would print a Boolean value, which one?
+print(bool("abc"))
+#Result:True
+
+#Exercise 5 Azamat
+#The statement below would print a Boolean value, which one?
+print(bool(0))
+#Result:False
+
+
