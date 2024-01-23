f80bf29c65d838558695e12f6e49bb697989cdee
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 17:45:52 +0600

    Create 2Operators.py

---
+++ 2Operators.py
@@ -0,0 +1,23 @@
+#Exercise 1 Azamat
+#Multiply 10 with 5, and print the result. Azamat
+print(10 * 5)
+
+#Exercise 2 Azamat
+#Divide 10 by 2, and print the result. Azamat
+print(10 / 2)
+
+#Exercise 3 Azamat
+#Use the correct membership operator to check if "apple" is present in the fruits object. Azamat
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#Exercise 4 Azamat
+#Use the correct comparison operator to check if 5 is not equal to 10. Azamat
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+#Exercise 5 Azamat
+#Use the correct logical operator to check if at least one of two statements is True. Azamat
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
