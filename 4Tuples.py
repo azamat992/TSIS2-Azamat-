83bb9f242a53d69e9595d8c84189158ea7faae74
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 18:03:46 +0600

    Create 4Tuples.py

---
+++ 4Tuples.py
@@ -0,0 +1,19 @@
+#Exercise 1 Azamat
+#Use the correct syntax to print the first item in the fruits tuple. Azamat
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+#Exercise 2 Azamat
+#Use the correct syntax to print the number of items in the fruits tuple. Azamat
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+#Exercise 3 Azamat
+#Use negative indexing to print the last item in the tuple. Azamat
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+#Exercise 4 Azamat
+#Use a range of indexes to print the third, fourth, and fifth item in the tuple. Azamat
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
