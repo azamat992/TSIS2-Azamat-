0147da326e9c495cd96d0432507adfbf74f2a736
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 17:58:34 +0600

    Create 3Lists.py

---
+++ 3Lists.py
@@ -0,0 +1,39 @@
+#Exercise 1 Azamat
+#Print the second item in the fruits list. Azamat
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#Exercise 2 Azamat
+#Change the value from "apple" to "kiwi", in the fruits list. Azamat
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi"
+
+#Exercise 3 Azamat
+#Use the append method to add "orange" to the fruits list. Azamat
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#Exercise 4 Azamat
+#Use the insert method to add "lemon" as the second item in the fruits list. Azamat
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#Exercise 5 Azamat
+#Use the remove method to remove "banana" from the fruits list. Azamat
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#Exercise 6 Azamat
+#Use negative indexing to print the last item in the list. Azamat
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#Exercise 7 Azamat
+#Use a range of indexes to print the third, fourth, and fifth item in the list. Azamat
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#Exercise 8 Azamat
+#Use the correct syntax to print the number of items in the list. Azamat
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
