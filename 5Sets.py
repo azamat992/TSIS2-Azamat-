35db1c00bea13ca22a90544bec7f7dbc3832d99b
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 18:06:58 +0600

    Create 5Sets.py

---
+++ 5Sets.py
@@ -0,0 +1,26 @@
+#Exercise 1 Azamat
+#Check if "apple" is present in the fruits set. Azamat
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#Exercise 2 Azamat
+#Use the add method to add "orange" to the fruits set. Azamat
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+#Exercise 3 Azamat
+#Use the correct method to add multiple items (more_fruits) to the fruits set. Azamat
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+#Exercise 4 Azamat
+#Use the remove method to remove "banana" from the fruits set. Azamat
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+#Exercise 5 Azamat
+#Use the discard method to remove "banana" from the fruits set. Azamat
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
