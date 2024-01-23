3254285c4ae6195273922598b4a2c8bae4856080
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-23 18:25:25 +0600

    Create 7If...Else.py

---
+++ 7If...Else.py
@@ -0,0 +1,62 @@
+#Exercise 1 Azamat
+#Print "Hello World" if a is greater than b. Azamat
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
+
+#Exercise 2 Azamat
+#Print "Hello World" if a is not equal to b. Azamat
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
+
+#Exercise 3 Azamat
+#Print "Yes" if a is equal to b, otherwise print "No". Azamat
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
+
+#Exercise 4 Azamat
+#Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3". Azamat
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+#Exercise 5 Azamat
+#Print "Hello" if a is equal to b, and c is equal to d. Azamat
+if a == b and c == d:
+  print("Hello")
+
+#Exercise 6 Azamat
+#Print "Hello" if a is equal to b, or if c is equal to d. Azamat
+if a == b or c == d:
+  print("Hello")
+
+#Exercise 7 Azamat
+#Complete the code block, print "YES" if 5 is larger than 2. Azamat
+if 5 > 2:
+    print("YES")
+
+#Exercise 8 Azamat
+#Use the correct one line short hand syntax to print "YES" if a is equal to b, otherwise print("NO"). Azamat
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+
+#Exercise 9 Azamat
+#Use an if statement to print "YES" if either a or b is equal to c. Azamat
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+  print("YES")
